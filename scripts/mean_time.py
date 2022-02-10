89eee0d6f2b3d8af6590160b79f7ab68271ae8ee
Tim Hagemann <tim@eternalwings.de>
2022-02-10 10:19:49 +0100

    Allow changing variables for grid in time mean

--- scripts/mean_time.py
+++ scripts/mean_time.py
@@ -8,6 +8,9 @@ class MeanTimeTest(scripts.test.Test):
     def get_default_args(self):
         return {
             'variable': 'tp',
+            'latitude': 'latitude',
+            'longitude': 'longitude',
+            'time': 'time',
             'time_range': None,
             'dask': '0',
             'time_chunk': '100'
@@ -22,16 +25,19 @@ class MeanTimeTest(scripts.test.Test):
             is_zarr = True
 
         if self.args['dask'] == '1':
-            dataset = opener(location, chunks={'time': int(self.args['time_chunk'])})
+            dataset = opener(location, chunks={self.args['time']: int(self.args['time_chunk'])})
         else:
             dataset = opener(location)
         args = self.args
-        context.start()
         var = dataset[args['variable']]
+        selection_def = {
+            args['longitude']: 30,
+            args['latitude']: 30
+        }
         if args['time_range'] != None:
-            selection = var.isel(longitude=30, latitude=50, time=range(0, int(args['time_range']) * 365))
-        else:
-            selection = var.isel(longitude=30, latitude=50)
+            selection_def[args['time']] = range(0, int(args['time_range']) * 365)
+        context.start()
+        selection = var.isel(selection_def)
         mean = selection.mean()
         if self.args['dask'] == '1' or is_zarr:
             mean = mean.compute()
